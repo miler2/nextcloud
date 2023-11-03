eab67676e6a0cd4f15867416783d65410852fedb
Ubuntu <ubuntu@ip-172-31-57-83.ec2.internal>
2023-11-03 17:30:20 +0000

    He quitado la instalación de apache2, php y varios

--- install.sh
+++ install.sh
@@ -10,18 +10,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 apt install docker-ce -y
 
-#Instalamos el servidor web Apache
-apt install apache2 -y
-
-#Instalamos el sistema gestor de bases de datos MySQL
-apt install mysql-server -y
-
-#Instalamos php
-apt install php libapache2-mod-php php-mysql -y
-
-#Reiniciamos el servidor para aplicar los cambios
-systemctl restart apache2
-
 #Modificamos el propietario y el grupo del directorio /var/www/html para
 #que la persona que abra esta página desde la web tenga permisos.
 #El comando "-R" para hacer los cambios de forma recursiva.
